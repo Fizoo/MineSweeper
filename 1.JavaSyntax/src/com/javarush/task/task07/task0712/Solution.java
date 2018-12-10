0d4d1791e52c0ea33f691aa954b1da9adac1294f
Oleg Mykytchuk <oleg.mykytchuk@gmail.com>
2018-12-10 20:35:19 +0200

    "F2 commit"

---
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0712/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task07.task0712;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+/* 
+Самые-самые
+Создай список строк.
+2. Добавь в него 10 строчек с клавиатуры.
+3. Узнай, какая строка в списке встретится раньше: самая короткая или самая длинная.
+Если таких строк несколько, то должны быть учтены самые первые из них.
+4. Выведи на экран строку из п.3. Должна быть выведена одна строка.
+
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+        String max = Collections.max(list, Comparator.comparing(String::length));
+        String min = Collections.min(list, Comparator.comparing(String::length));
+        int result = list.indexOf(max) > list.indexOf(min) ? list.indexOf(min) : list.indexOf(max);
+        System.out.println(list.get(result));
+
+        //напишите тут ваш код
+    }
+}
